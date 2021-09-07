6c1249dff03b9b57f934aa73038fb26ca156a783
Tajul Islam <tajul.islam.saikat133@gmail.com>
2021-09-07 21:46:50 +0600

    New component folder sign added and change login and registration page

---
+++ components/sign/SignIn.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="bg-gray-100 font-solaiman text-main">
+    <div class="container mx-auto min-h-screen py-10 flex flex-wrap items-center justify-center">
+      <!-- logo start -->
+      <div class="p-5 w-full md:w-1/2 flex justify-center">
+        <img
+          src="/logoft.png"
+          alt=""
+        >
+      </div>
+      <!-- logo end -->
+
+      <!-- form start -->
+      <div class="w-full md:w-1/3 bg-white p-4 mx-7 my-10 md:my-0 md:mx-0 rounded-md shadow-md">
+        <div class="w-full text-center pb-5 pt-2">
+          <h2 class="font-bold text-3xl">একাউন্টে প্রবেশ করুন</h2>
+        </div>
+        <form class="w-full">
+          <div class="w-full px-3 mb-6">
+            <label
+              class="block capitalize tracking-wide text-md font-bold mb-2"
+              for="grid-first-name"
+            >
+              Username/Email
+            </label>
+            <input
+              class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
+              id="grid-first-name"
+              type="text"
+              placeholder="shishurjonnoamra"
+            >
+          </div>
+          <div class="w-full px-3 mb-6">
+            <label
+              class="block capitalize tracking-wide text-md font-bold mb-2"
+              for="grid-password"
+            >
+              Password
+            </label>
+            <input
+              class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
+              id="grid-password"
+              type="password"
+              placeholder="******************"
+            >
+            <p class="text-red-700 text-xs">The password that you've entered is incorrect. Forgotten password?</p>
+          </div>
+
+          <div class="px-3 flex flex-col justify-center pb-5 space-y-2">
+            <button
+              class="shadow bg-main hover:bg-primary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Login
+            </button>
+            <a href="#">Forgotten password?</a>
+          </div>
+          <hr>
+          <div class=" px-3 flex justify-center py-5">
+            <button
+              class="shadow bg-main hover:bg-primary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Create Donor Account
+            </button>
+          </div>
+        </form>
+        <div class="w-full text-center">
+          <a
+            href="#"
+            class="text-md hover:text-primary"
+          >মূল পাতায় ফিরে যান</a>
+        </div>
+      </div>
+      <!-- form end -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
--- pages/registration.vue
+++ pages/registration.vue
@@ -1,136 +1,19 @@
 <template>
-  <div class="bg-gray-100 font-solaiman text-main">
-    <div class="container mx-auto min-h-screen py-10 flex flex-wrap items-center justify-center">
-      <!-- logo start -->
-      <div class="p-5 w-full md:w-1/2 flex justify-center">
-        <img
-          src="/logoft.png"
-          alt=""
-        >
-      </div>
-      <!-- logo end -->
-
-      <!-- form start -->
-      <div class="w-full md:w-1/2 bg-white p-4 mx-7 my-10 md:my-0 md:mx-0 rounded-md shadow-md">
-        <div class="w-full text-center pb-5 pt-2">
-          <h2 class="font-bold text-3xl">ডোনার হিসেবে রেজিস্ট্রেশন করুন</h2>
-        </div>
-        <form class="w-full">
-          <div class="flex flex-wrap mb-6">
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block capitalize tracking-wide text-md font-bold mb-2"
-                for="grid-first-name"
-              >
-                First Name
-              </label>
-              <input
-                class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
-                id="grid-first-name"
-                type="text"
-                placeholder="shishurjonnoamra"
-              >
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block capitalize tracking-wide text-md font-bold mb-2"
-                for="grid-first-name"
-              >
-                Lastname
-              </label>
-              <input
-                class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
-                id="grid-first-name"
-                type="text"
-                placeholder="shishurjonnoamra"
-              >
-            </div>
-          </div>
-          <div class="w-full flex flex-wrap mb-6">
-            <div class="w-1/2 px-3">
-              <label
-                class="block capitalize tracking-wide text-md font-bold mb-2"
-                for="email"
-              >
-                Email
-              </label>
-              <input
-                class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
-                id="email"
-                type="email"
-                placeholder="example@mail.com"
-              >
-            </div>
-            <div class="w-1/2 px-3">
-              <label
-                class="block capitalize tracking-wide text-md font-bold mb-2"
-                for="phone"
-              >
-                Phone
-              </label>
-              <input
-                class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
-                id="phone"
-                type="number"
-                placeholder="+880-1234-567-890"
-              >
-            </div>
-          </div>
-          <div class="w-full px-3 mb-6">
-            <label
-              class="block capitalize tracking-wide text-md font-bold mb-2"
-              for="grid-password"
-            >
-              Password
-            </label>
-            <input
-              class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-main"
-              id="grid-password"
-              type="password"
-              placeholder="******************"
-            >
-            <p class="text-red-700 text-xs">The password that you've entered is incorrect. Forgotten password?</p>
-          </div>
-          <div class="px-3 flex justify-center pb-5 space-y-2">
-            <button
-              class="shadow bg-main hover:bg-primary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="button"
-            >
-              Register as Donor
-            </button>
-          </div>
-          <hr>
-          <div class=" px-3 flex flex-col justify-center items-center space-y-2 py-5 capitalize">
-            <div>
-              <p> already have an account? goto</p>
-            </div>
-            <div>
-              <button
-                class="shadow bg-main hover:bg-primary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                type="button"
-              >
-                Login as Donor
-              </button>
-            </div>
-          </div>
-        </form>
-        <div class="w-full text-center">
-          <a
-            href="#"
-            class="text-md hover:text-primary"
-          >মূল পাতায় ফিরে যান</a>
-        </div>
-      </div>
-      <!-- form end -->
-    </div>
+  <div>
+    <SignUp />
   </div>
 </template>
 
 <script>
+import SignUp from '@/components/sign/SignUp.vue'
+
 export default {
   layout: 'sign',
+  components: {
+    SignUp,
+  },
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
